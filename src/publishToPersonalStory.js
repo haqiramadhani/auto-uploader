2619eee8c188323e381754965d66dfc0729d1300
Haqi Ramadhani <hakiramadhani@gmail.com>
2021-01-12 09:48:41 +0700

    Publish Personal Text Story

--- src/publishToPersonalStory.js
+++ src/publishToPersonalStory.js
@@ -1,5 +1,11 @@
 const publishToPersonalStory = async (page, text) => {
     await page.waitForNavigation();
     await page.goto("https://www.facebook.com/stories/create");
-
+    await page.waitForSelector("[aria-label=\"Stories Creation Preview\"] > div > div > div > div > div:nth-child(3) > div");
+    await page.click("[aria-label=\"Stories Creation Preview\"] > div > div > div > div > div:nth-child(3) > div");
+    await page.waitForSelector("textarea");
+    await page.type("textarea", text);
+    await page.click("[aria-label=\"Share to Story\"]");
 }
+
+module.exports = publishToPersonalStory;
